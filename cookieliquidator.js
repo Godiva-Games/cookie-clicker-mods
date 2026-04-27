9d865fa7b60a597bb108695dad29825d7216cc34
Aaron P Johnson <aaron.p.johnson91@gmail.com>
2026-04-27 15:04:28 -0600

    Update cookieliquidator.js
    Skeleton init

--- cookieliquidator.js
+++ cookieliquidator.js
@@ -1,1 +1,89 @@
+// ==UserScript==
+// @name         CookieLiquidator
+// @namespace    CookieLiquidator
+// @version      0.1.0
+// @description  Cookie Clicker Stock Market Helper
+// @author       SynGodiva
+// @match        https://orteil.dashnet.org/cookieclicker/
+// @match        http://orteil.dashnet.org/cookieclicker/
+// @homepageURL  https://github.com/Godiva-Games/cookie-clicker-mods
+// @supportURL   https://github.com/Godiva-Games/cookie-clicker-mods
+// @updateURL    https://github.com/Godiva-Games/cookie-clicker-mods
+// @icon         https://github.com/Godiva-Games/cookie-clicker-mods
+// @license      MIT
+// @grant        none
+// ==/UserScript==
 
+// Run
+(async function() {
+    'use strict';
+    
+    // Wait for game to load
+    let tries = 0
+    let maxTries = 100
+    while(tries < maxTries)
+    {
+        if (window.Game && Game.ready)
+        {
+
+            // Register mod
+            Game.registerMod('CookieLiquidator',
+            {
+                // Variables
+                // -- Options
+                liquidatorLoopFrequency
+                liquidatorActivityReportFrequency
+                
+                // Constants
+                // -- Strings
+                liquidatorGreeting = 'Becoming exit liquidity in 3..2..1..'
+                
+                
+                async function startLiquidating()
+                {
+                    if (!Game.Objects['Bank'].minigame)
+                    {
+                        console.log('===$$$=== Stock Market minigame has not initialised yet! Will try again in 500 ms.');
+                        await new Promise(r => setTemout(r, 500))
+                        return
+                    }
+                    else
+                    {
+                        console.log('===$$$=== ')
+                        console.log('===$$$=== CookieLiquidator initialised at ' + new Date())
+                        console.log('===$$$=== with main options as follows:')
+                        console.log('===$$$=== Logic loop frequency: ' + liquidatorTimeBeautifier(liquidatorLoopFrequency))
+                        console.log('===$$$=== Profit/Loss/Efficiency report frequency: ' + liquidatorTimeBeautifier(liquidatorActivityReportFrequency))
+                        console.log('===$$$=== ')
+                        Game.Notify(`CookieLiquidator is ready`,liquidatorGreeting,[35,33],false)
+                        console.log(stockList.check)
+                    }
+                },
+                init:function()
+                {
+                    this.startLiquidating();
+                },
+                
+                save:function()
+                {
+                
+                },
+                
+                load:function(str)
+                {
+                
+                },
+            });
+        }
+        else
+        {
+            await new Promise(r => setTimeout(r, 500))
+            tries++
+        }
+    }
+    if (tries >= maxTries)
+    {
+        console.error('CookieLiquidator couldn\'t load properly!')
+        return
+    }
+})()
